876a1a63bb1dea83a4e2a665caf256d80c3d0190
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2024-10-22 14:28:39 +0530

    getting data dynamically in signup route

--- src/app.js
+++ src/app.js
@@ -5,16 +5,10 @@ const PORT = 4000
 app.use(express.json())
 const User = require("./models/user")
 
-app.post("/signup", (req,res) => {
-    const user = new User({
-        firstName: "Maurya",
-        lastName: "Ratnesh",
-        email: "shailesh@gmail.com",
-        password: "shailesh1234",
-        age: 24
-    })
+app.post("/signup", async(req,res) => {
+    const user = new User(req.body)
     try {
-        user.save()
+        await user.save()
         res.send("user created successfully")
     } catch (error) {
         res.status(400).send("Error while saving the user",error)
